4e69334f2094bb9738a566fadc679d1b5b1cc1dc
illjah <illjah@bk.ru>
2019-07-04 07:48:27 +0300

    AWS-507: fb-reservation-dropdown addded

---
+++ resources/vue/fb-reservation-dropdown.vue
@@ -0,0 +1,218 @@
+<template>
+    <div
+        class="fb-date fb-element"
+        @focusout="_focusoutHandler"
+    >
+        <fb-error-wrap
+            :open="showTooltip"
+            :error="error"
+            @clickTooltip="clickTooltip"
+        >
+            <label
+                class="fb-input fb-date__fb-input"
+                :class="{ 'fb-input_disabled': isDisabled, 'fb-input_active': dateFormatted, 'fb-input_error': hasError, 'animated shake': shake }"
+            >
+
+                <span
+                    class="fb-input__label fb-input__label_field"
+                >
+                    {{ label }}
+                </span>
+            
+            
+                <input
+                    class="fb-input__field"
+                    :class="{ 'is-focusable': isFocusable, 'in-focus': inFocus, 'has-label': label }"
+                    :disabled="isDisabled"
+                    :value="dateFormatted"
+                    readonly
+                    @focus="showPicker"
+                    @blur="_focusoutHandler"
+                    ref="element"
+                >
+            </label>
+        </fb-error-wrap>
+
+        <div
+            class="fb-date__picker is-reservation"
+            :class="{'is-opened': isOpened}"
+        >
+
+            <div class="fb-reservation">
+
+                <!-- calendar -->
+                <div
+                    class="fb-reservation__calendar"
+                    :class="{'is-visible': ! showTime}"
+                >
+
+                    <div class="ui-calendar__header">
+                        <time
+                            class="ui-calendar__caption"
+                            :datetime="`${year}-${month + 1}`"
+                        >
+                            {{ monthName }} {{ year }}
+                        </time>
+                        <button
+                            class="ui-calendar__button is-prev"
+                            :disabled="prevButtonDisabled"
+                            type="button"
+                            @click="setMonth(month - 1)"
+                        >
+                            previouse month
+                        </button>
+                        <button
+                            class="ui-calendar__button is-next"
+                            type="button"
+                            @click="setMonth(month + 1)"
+                        >
+                            next month
+                        </button>
+                    </div>
+
+                    <ui-calendar
+                        class="fb-reservation__ui-calendar"
+                        v-bind="{month, year, firstDay}"
+                        :value="date"
+                        :lang="{weekdays: $lang.FORMS_DATEPICKER.days}"
+                    >
+                        <button
+                            slot="day"
+                            slot-scope="{date: calendarDate, timestamp, isSelected, isEdge}"
+                            class="ui-calendar__day"
+                            :class="{
+                                'is-edge': isEdge,
+                                'is-selected': isSelected,
+                                'is-today': timestamp === today,
+                                'is-disabled': !availableDays[timestamp]
+                            }"
+                            :disabled="!availableDays[timestamp]"
+                            type="button"
+                            :data-date="isEdge ? null : timestamp"
+                            v-on="isEdge || !availableDays[timestamp] ? {} : { click: _setDate }"
+                        >
+                            {{ calendarDate.getDate() }}
+                        </button>
+                    </ui-calendar>
+                </div><!-- / calendar -->
+
+                <!-- time range -->
+                <div
+                    v-show="!! date"
+                    class="fb-reservation__time"
+                    :class="{'is-visible': showTime}"
+                >
+                    <div class="ui-calendar__header">
+
+                        <button
+                            class="ui-calendar__button is-prev fb-reservation__hide-time"
+                            type="button"
+                            @click="showTime = false"
+                        >
+                            show datepicker
+                        </button>
+
+                        <time
+                            class="ui-calendar__caption"
+                            :datetime="dateInfo.datetime"
+                        >
+                            <slot name="time-header" v-bind="dateInfo">
+                                {{ dateInfo.weekdayName }}, {{ dateInfo.monthName }} {{ dateInfo.day }}
+                                <span class="fb-reservation__caption-year">{{ dateInfo.year }}</span>
+                            </slot>
+                        </time>
+                    </div>
+                    <div class="ui-calendar__time-range">
+                        <ul v-if="availableTime" @click="_setTime">
+                            <li
+                                v-for="{time_start, status, format24h, format12h} in availableTime"
+                                :key="time_start"
+                            >
+                                <button
+                                    type="button"
+                                    class="ui-calendar__button"
+                                    :class="[{
+                                        'is-selected': time === format24h,
+                                        'is-disabled': status !== 'available'
+                                    }, 'is-' + status]"
+                                    :disabled="status !== 'available'"
+                                    :data-time="format24h"
+                                >
+                                    {{ is24hFormat ? format24h : format12h }}
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- / time range -->
+
+                <!-- loading overlay -->
+                <div class="fb-reservation__loading" v-show="isLoading">
+                    <slot name="loading">
+                        Loading...
+                    </slot>
+                </div>
+            </div>
+        </div>
+
+        <fb-input
+            :name="realName"
+            type="hidden"
+            @error="err => inputError = err"
+        />
+    </div>
+</template>
+
+
+<script>
+import dateMixin from '../js/mixins/fb-date.js'
+import focusMixin from '../js/mixins/fb-focus.js'
+import FbReservation from './fb-reservation.vue'
+
+export default {
+
+    name: 'fb-reservation-dropdown',
+
+
+    extends: FbReservation,
+
+
+    mixins: [ focusMixin, dateMixin ],
+
+
+    props: {
+
+        format: {
+            type: String,
+            default: 'MM/DD/YYYY HH:mm'
+        },
+
+        label: String
+    },
+
+
+    data() {
+        return {
+            inputError: undefined
+        }
+    },
+
+
+    computed: {
+
+        dateFormatted() {
+            return this.selected && this.$dayjs(this.selected).format(this.format) || ''
+        }
+    },
+
+
+    watch: {
+
+        selected(newValue, oldValue) {
+
+            if ( newValue /*&& ! oldValue*/ ) {
+                this.hidePicker()
+            }
+        }
+    }
+}
+</script>
